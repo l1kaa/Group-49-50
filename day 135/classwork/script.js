913c4ea730634348b7e01eca4aac118862d83d46
l1kaa <chxikvadzelikuna1@gmail.com>
2025-08-25 00:40:35 +0400

    ★

--- day 135/classwork/script.js
+++ day 135/classwork/script.js
@@ -1,1 +1,22 @@
-// No cw.
+function randomPromise() {
+    const delay = Math.floor(Math.random() * 10 + 1) * 1000; // 1–10 sec
+    return new Promise(resolve => {
+        setTimeout(() => {
+        resolve(`Promise resolved after ${delay / 1000} seconds`);
+        }, delay);
+    });
+}
+
+// rejects after 5 seconds
+function timeoutPromise(ms = 5000) {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+        reject("Promise timed out.");
+        }, ms);
+    });
+}
+
+
+Promise.race([randomPromise(), timeoutPromise()])
+    .then(result => console.log(result))
+    .catch(error => console.error(error));
---
+++ day 137/homework/script.js
@@ -0,0 +1,3 @@
+//  https://www.youtube.com/watch?v=8aGhZQkoFbQ      კიდევ ერთხელ უყურეთ ამ ვიდეოს
+
+// Done.
